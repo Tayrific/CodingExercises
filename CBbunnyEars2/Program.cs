76925249d9c23866c9ec21caeed7a20452bb3f0a
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-05-22 14:26:52 +0100

    Coding Bat bunnyEars2

---
+++ CBbunnyEars2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CBbunnyEars2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4; // Example number of bunnies
+            int totalEars = CountBunnyEars(n);
+            Console.WriteLine("Total ears for {0} bunnies: {1}", n, totalEars);
+        }
+
+        public static int CountBunnyEars(int n)
+        {
+            if (n == 0)
+            { 
+                return 0; //no bunnies
+            }
+
+            if (n % 2 == 1)
+            {            
+                return 2 + CountBunnyEars(n - 1); // Add 2 for odd bunny then recursively call for next bunny
+            }
+            else
+            {          
+                return 3 + CountBunnyEars(n - 1); // Add 3 for even bunny then recursively call for next bunny
+            }
+
+        }
+    }
+}
