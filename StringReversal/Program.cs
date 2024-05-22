b3fe189e5341532efd60e84332ccb52959edfd83
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-05-22 11:24:38 +0100

    Problem 5 complete

---
+++ StringReversal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace StringReversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            Console.WriteLine("enter a string you would like in reverse: ");
+            input = Console.ReadLine();
+
+            //reversing string
+
+            StringBuilder reverse = new StringBuilder();
+            for (int i = input.Length - 1; i >= 0; i--) //starting at the last char
+            {
+                reverse.Append(input[i]); //adding the char to stringBuilder
+            }
+
+            string reversedInput = reverse.ToString();
+            Console.WriteLine(reversedInput); //output reversed
+
+        }
+    }
+}
