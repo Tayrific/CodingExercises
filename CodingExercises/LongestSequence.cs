27ab198f5cc36698a0e77c9cf408a6d231833594
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-05-22 10:12:35 +0100

    Add project files.

---
+++ CodingExercises/LongestSequence.cs
@@ -0,0 +1,33 @@
+﻿namespace CodingExercises
+{
+    internal class LongestSequence
+    {
+        static void Main(string[] args)
+        {
+            List<int> sequence = new List<int> {1,2,1,1,0,3,1,0,0,2,3,1,0,0,0,0,2,1,0,3,1,0,0,0,6,1,3,0,0,0};
+            Console.WriteLine("Longest sequence of days without sale: {0}",longestSeq(sequence));
+            Console.ReadLine();
+            
+        }
+
+        static int longestSeq(List<int> sequence)
+        {
+            int max = 0;
+            int current = 0;
+
+            foreach (int num in sequence)
+            {
+                if (num == 0) {
+                    current++;
+                    if (current > max) {
+                        max = current;
+                    }
+                } else {
+                    current = 0;
+                }
+            }
+
+            return max;
+        }
+    }
+}
