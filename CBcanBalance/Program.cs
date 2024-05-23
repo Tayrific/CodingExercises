b2ed76ba267e3688432b022b401e1d38199609c7
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-05-23 09:53:14 +0100

    Higher Lower problem

---
+++ CBcanBalance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CBcanBalance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 1, 2, 1 };
+            int[] nums2 = { 2, 1, 1, 2, 1 };
+
+            bool checkNums = canBalance(nums);
+            Console.WriteLine(checkNums); // Should print: True
+            bool checkNums2 = canBalance(nums2);
+            Console.WriteLine(checkNums2); // Should print: False
+        }
+
+        public static bool canBalance(int[] nums)
+        {
+            // Iterate through each possible split point
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int sum1 = 0;
+                int sum2 = 0;
+
+                // Calculate the sum of the first part
+                for (int j = 0; j < i; j++)
+                {
+                    sum1 += nums[j];
+                }
+
+                // Calculate the sum of the second part
+                for (int j = i; j < nums.Length; j++)
+                {
+                    sum2 += nums[j];
+                }
+
+                // Check if the two sums are equal
+                if (sum1 == sum2)
+                {
+                    return true;
+                }
+            }
+
+            // If no split point is found that balances the array, return false
+            return false;
+        }
+    }
+}
---
+++ HigherLower/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace HigherLower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Find the number between 1-99");
+
+            Random rng = new Random();
+            int rand = rng.Next(100); // number between 0 and 99
+            
+            GuessNumber(rand);
+
+        }
+
+        public static void GuessNumber(int rand)
+        {
+            int input = 0;
+            int guesses = 6;
+
+            while (guesses > 0 && input != rand)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Guess a number: "); // user guesses number
+                Console.WriteLine(" ");
+                input = Convert.ToInt32(Console.ReadLine());
+                guesses--;
+
+                if (rand > input) //deciding if the random number is higher or lower
+                {
+                    Console.WriteLine("The number is higher");
+                }
+                else if (rand < input)
+                {
+                    Console.WriteLine("The number is lower");
+                }
+            }
+
+            if (input == rand)
+            {
+                Console.WriteLine("You won!");
+            }
+            else
+            {
+                Console.WriteLine("You lost!");
+            }
+
+        }
+    }
+}
